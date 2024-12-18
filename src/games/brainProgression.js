e0ea0cf9b4e2020209b3d4775fb7ee91345707eb
Выставкина Юлия <yulya.reso@mail.ru>
2024-12-18 13:32:05 +0300

    update progression game

--- src/games/brainProgression.js
+++ src/games/brainProgression.js
@@ -36,7 +36,7 @@ const brainProgression = () => {
       correctAnswer = i;
     } else {
       console.log(
-        `${Question.toString()} is wrong answer ;(. Correct answer was ${result.toString()}. \nLet\'s try again, ${userName}!`,
+        `\'${(Question.toString())}\'  is wrong answer ;(. Correct answer was \'${result.toString()}\'. \nLet\'s try again, ${userName}!`,
       );
       return;
     }
